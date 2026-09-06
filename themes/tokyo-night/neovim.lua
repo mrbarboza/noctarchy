b5570adef7b2fbcf3f139182a35cce8f378c5fbb
Miguel Barboza <14060805+mrbarboza@users.noreply.github.com>
2026-09-06 15:44:32 -0300

    Complete Tokyo-Night theme config files

--- themes/tokyo-night/neovim.lua
+++ themes/tokyo-night/neovim.lua
@@ -1,1 +1,22 @@
--- Neovim theme config
+-- Tokyo Night for Neovim
+-- https://github.com/folke/tokyonight.nvim
+
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night",
+    transparent = true,
+    terminal_colors = true,
+    styles = {
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = { italic = false },
+      variables = { italic = false },
+    },
+    colors = {
+      border = "#3b4261",
+    },
+  },
+}
